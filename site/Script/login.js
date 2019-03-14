d6f7f638ab6aca406748ac42ff2bb11d3ad2de05
nhocthaipro245 <nhocthaipro28@gmail.com>
2019-03-14 14:47:11 +0700

    Create login.js

---
+++ site/Script/login.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+function checkSubmit() {
+	var Studentname = f1.txtname.value;
+var b= f1.txtC.value;
+var c= f1.txtJava.value;
+var d= f1.txtdatabase.value;
+
+document.writeln("Studentname: "+Studentname);
+	document.writeln("C# Mark: " +b );
+	document.writeln("Java Mark: " +c );
+	document.writeln("Database Mark: " +d);
+	
+	
+}
